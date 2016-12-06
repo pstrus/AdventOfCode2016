34f39c539cf8db8bd0a7b3affc1d0e8077d76786
Jakub <jakub.pstrusinski@gmail.com>
2016-12-06 11:51:41 +0000

    Adding Day6 (both parts)

---
+++ Day6/Day6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day6
+{
+    class Day6
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("Input.txt");
+
+            //set to true for Part Two of the puzzle
+            bool PartTwo = true;
+
+            string FinalString = string.Empty ;
+
+            for (int i = 0; i < input[0].Length; i++)
+            {
+                string lettersInAColumn = new string(input.Select(s => s[i]).ToArray());
+                char mostCommonLetter;
+                if (PartTwo)
+                     mostCommonLetter = lettersInAColumn.GroupBy(s => s).OrderBy(s => s.Count()).First().Key;
+                else
+                    mostCommonLetter = lettersInAColumn.GroupBy(s => s).OrderByDescending(s => s.Count()).First().Key;
+
+                
+                FinalString += mostCommonLetter;
+            }
+
+
+            Console.WriteLine(FinalString);
+            Console.ReadKey();
+        }
+    }
+}
