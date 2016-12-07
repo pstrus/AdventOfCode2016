5430817134a907df7773d81e5444003393f1fd57
Jakub <jakub.pstrusinski@gmail.com>
2016-12-07 10:43:55 +0000

    Adding Part Two

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -12,9 +12,10 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            //count should be 2 with InputTest.txt;
+            //count should be 2 with InputTest.txt; and 3 with InputTestAba;
             var input = File.ReadAllLines("Input.txt");
-            int count = 0;
+            int countABBA = 0;
+            int countABA = 0;
             foreach (var item in input)
             {
 
@@ -22,14 +23,42 @@ namespace Day7
                 string pattern = @"\[(.*?)\]";
                 string[] result = Regex.Split(item, pattern);
 
+                string[] OutsideBracketsBits = result.Where((value, index) => index % 2 == 0).ToArray();
+                string[] InsideBracketsBits = result.Where((value, index) => index % 2 != 0).ToArray();
 
-                if (SupportsAbba(result.Where((value,index) => index % 2 ==0).ToArray()) && !SupportsAbba(result.Where((value, index) => index % 2 != 0).ToArray()))
-                    count++;
+                if (SupportsAbba(OutsideBracketsBits) && !SupportsAbba(InsideBracketsBits))
+                    countABBA++;
+       
+                if (SupportsAba(OutsideBracketsBits, InsideBracketsBits))
+                    countABA++;
+
 
             }
 
-            Console.WriteLine("IPs with TLS support: " + count);
+            Console.WriteLine("IPs with TLS support: " + countABBA);
+            Console.WriteLine("IPs with SSL support: " + countABA);
+
             Console.ReadKey();
+        }
+
+        private static bool SupportsAba(string[] outsideBracketsBits, string[] insideBracketsBits)
+        {
+
+
+            foreach (var outBit in outsideBracketsBits)
+            {
+                for (int i = 0; i <= outBit.Length - 3; i++)
+                {
+                    if (outBit[i] != outBit[i + 1] && outBit[i] == outBit[i + 2])
+                    {
+                        char[] c = { outBit[i+1], outBit[i], outBit[i+1] };
+                        string searchString = new string(c);
+                        if (insideBracketsBits.Any(s => s.Contains(searchString)))
+                            return true;
+                    }
+                }
+            }
+            return false;
         }
 
         public static bool SupportsAbba(string[] IPsArray)
