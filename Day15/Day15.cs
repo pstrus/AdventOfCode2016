3085ae03abc46470b0a40e68221f684e9f805c58
Jakub <jakub.pstrusinski@gmail.com>
2016-12-18 21:09:49 +0000

    Adding day15 and day17

---
+++ Day15/Day15.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day15
+{
+    class Day15
+    {
+        static void Main(string[] args)
+        {
+            int requestedSlot = 0;
+            int time = 0;
+            List<Disc> discList = new List<Disc>();
+
+            bool foundStartingTime = false;
+
+            //for inputTest.txt expected time-1 = 5
+            var input = File.ReadAllLines("Input.txt");
+            foreach (var line in input)
+            {
+                var items = line.Split(' ');
+                discList.Add(new Disc() { noOfPoisitons = int.Parse(items[3]), startingPosition = int.Parse(items[items.Length - 1].Split('.')[0])});
+
+            }
+
+            while (!foundStartingTime)
+            {
+
+                for (int i  = 0; i  < discList.Count; i ++)
+                {
+                    if ((discList[i].startingPosition + i + time + 1) % discList[i].noOfPoisitons != requestedSlot) break;
+                    if (i+1 == discList.Count) foundStartingTime = true;
+                }
+
+                time++;
+            }
+
+            Console.WriteLine(time - 1);
+            Console.ReadKey();
+
+        }
+        static void Main2(string[] args)
+        {
+            var input = new[] { 1, 17, 0, 7, 2, 19, 0, 5, 0, 3, 5, 13, 0, 11 };
+            Console.WriteLine(Enumerable.Range(0, int.MaxValue).TakeWhile(time => Enumerable.Range(0, 7).Select(i => (input[2 * i] + time + i + 1) % input[2 * i + 1]).Count(a => a != 0) != 0).Last() + 1);
+            Console.ReadKey();
+        }
+    }
+
+    struct Disc
+    {
+        public int startingPosition;
+        public int noOfPoisitons;
+    }
+
+
+
+
+}
+
---
+++ Day17/Day18.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day17
+{
+    class Day18
+    {
+        static void Main(string[] args)
+        {
+            //solution inspired by the one posted by JakDrako in here:   
+            Point targetPoint = new Point(3, -3);
+            Queue<State> queue = new Queue<State>();
+
+            string input = "awrkjxxr";
+            string openChars = "bcdef";
+            List<string> possiblePath = new List<string>();
+            queue.Enqueue(new State("", new Point(0,0)));
+
+            while (queue.Count >0)
+            {
+                State currentState = queue.Dequeue();
+                if (currentState.position == targetPoint)
+                {
+                    Console.WriteLine(currentState.path);
+                    possiblePath.Add(currentState.path);
+                }
+                else
+                {
+                    string hash = GetMD5Hash(input + currentState.path);
+                    if (openChars.Contains(hash[0]) && currentState.position.Y < 0) queue.Enqueue(new State(currentState.path + 'U', new Point(currentState.position.X, currentState.position.Y + 1)));
+                    if (openChars.Contains(hash[1]) && currentState.position.Y > targetPoint.Y) queue.Enqueue(new State(currentState.path + 'D', new Point(currentState.position.X, currentState.position.Y - 1)));
+                    if (openChars.Contains(hash[2]) && currentState.position.X > 0) queue.Enqueue(new State(currentState.path + 'L', new Point(currentState.position.X-1, currentState.position.Y )));
+                    if (openChars.Contains(hash[3]) && currentState.position.X < targetPoint.X) queue.Enqueue(new State(currentState.path + 'R', new Point(currentState.position.X + 1 , currentState.position.Y)));
+                }            
+
+            }
+            Console.WriteLine("Shortest path = " + possiblePath.OrderBy(s => s.Length).First());
+            Console.WriteLine("Longest path = " + possiblePath.OrderByDescending(s => s.Length).First());
+            Console.WriteLine("Longest path length = " + possiblePath.OrderByDescending(s => s.Length).First().Length);
+            Console.ReadKey();
+        }
+
+
+
+        static string GetMD5Hash(string input)
+        {
+
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(input);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+            string encoded = BitConverter.ToString(hash)
+                .Replace("-", string.Empty)
+                .ToLower();
+            return encoded;
+        }
+
+    }
+
+    public class State
+    {
+        public State (string Path, Point point)
+        {
+            this.path = Path;
+            position = point;
+        }
+        public string path = string.Empty;
+        public Point position = new Point(0, 0);
+    }
+
+}
