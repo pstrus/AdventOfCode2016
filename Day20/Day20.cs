ca67d2880c15c787cb6570b2d1b29ce824c9e27a
Jakub <jakub.pstrusinski@gmail.com>
2016-12-20 20:34:40 +0000

    Adding Day20

---
+++ Day20/Day20.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Day20
+{
+    class Day20
+    {
+        static void Main(string[] args)
+        {
+            long current = 0;
+            List<long> results = new List<long>();
+
+            var input = File.ReadAllLines("Input.txt");
+            var lastRow = input.Last();
+
+            while (current <= uint.MaxValue)
+            {
+                bool increase = true;
+                foreach (var line in input)
+                {
+                    var boundries = line.Trim().Split('-');
+                    long lowerBoundry = long.Parse(boundries[0]);
+                    long upperBoundry = long.Parse(boundries[1]);
+              
+                    if (current >= lowerBoundry && current <= upperBoundry) 
+                    {
+                        current = upperBoundry + 1;
+                        increase = false;
+                        break;
+                    }
+
+                    if (line == lastRow)
+                        results.Add(current);
+                }
+                if(increase) current++;
+
+            }
+            Console.WriteLine("First=" + results.First());
+            Console.WriteLine("Count=" + results.Count());
+            Console.ReadKey();
+        }
+    } 
+}
+
+
+
